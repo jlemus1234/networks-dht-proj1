861ee25705ae6982d781b3397d96453b72fb8c79
Emily Colwell <emily.colwell@tufts.edu>
2018-04-26 08:05:50 -0400

    Finger table for strings of size HASH_LEN + 1 working

--- newTest.c
+++ newTest.c
@@ -1,13 +1,16 @@
-
 #include <stdio.h>
+#include <string.h>
+#include "hashing.h"
 
-#define num_entries 10
+#define num_entries 5
+#define HASH_LEN 40
 int max_index = num_entries - 1;
+
 
 int pred;
 int largest_node;
-int self;
-int finger_table[num_entries];
+char *self;
+char finger_table[num_entries][HASH_LEN + 1];
 /*int sockfd;
 struct sockaddr_in serveraddr;
 /*
@@ -16,21 +19,21 @@ void init(int portno);
 */
 
 
-int next_hop(int key);
+int next_hop(char key[HASH_LEN + 1]);
 
 
 int main()
 {
 	int num = 1;
 
-	self = 0;
+	self = "0000000000000000000000000000000000000000\0";
 	largest_node = 1;
 	pred = 0;
 
 	int split_val = 6;
 
 	fprintf(stderr, "Making Table\n");
-	for(int i = split_val; i < num_entries; i++) {
+/*	for(int i = split_val; i < num_entries; i++) {
 		finger_table[i] = num;
 		num *= 2;
         }
@@ -40,27 +43,23 @@ int main()
 	for(int i = 0; i < split_val; i++) {
 		finger_table[i] = num;
 		num *= 2;
-        }
+                }*/
+
+	memcpy( finger_table[0], "1234567890123456789012345678901234567890\0", HASH_LEN + 1); 
+
+	memcpy( finger_table[1], "2234567890123456789012345678901234567890\0", HASH_LEN + 1); 
+
+	memcpy( finger_table[2], "3234567890123456789012345678901234567890\0", HASH_LEN + 1); 
+
+	memcpy( finger_table[3], "4234567890123456789012345678901234567890\0", HASH_LEN + 1); 
+
+	memcpy( finger_table[4], "5234567890123456789012345678901234567890\0", HASH_LEN + 1); 
 
 
 	for(int i = 0; i < num_entries; i++) {
-		fprintf(stderr, "entry %d: %d\n",i,finger_table[i]);
+		fprintf(stderr, "entry %d: %s\n", i, finger_table[i]);
         }
-	fprintf(stderr, "Next hop for %d is %d\n",2,next_hop(2));
-
-	fprintf(stderr, "Next hop for %d is %d\n",6,next_hop(6));
-
-	fprintf(stderr, "Next hop for %d is %d\n",12,next_hop(12));
-
-	fprintf(stderr, "Next hop for %d is %d\n",30,next_hop(30));
-
-	fprintf(stderr, "Next hop for %d is %d\n",127,next_hop(127));
-
-	fprintf(stderr, "Next hop for %d is %d\n",129,next_hop(129));
-
-	fprintf(stderr, "Next hop for %d is %d\n",511,next_hop(511));
-
-	fprintf(stderr, "Next hop for %d is %d\n",514,next_hop(514));
+	fprintf(stderr, "Next hop for %s is %s\n",  "4000000000000000000000000000000000000000\0", finger_table[next_hop( "4000000000000000000000000000000000000000\0")]);
 
 	
 /*	if(argc == 2) {
@@ -85,23 +84,23 @@ void init(int portno, str hostname, int hostport) {
 }
 */
 
-int next_hop(int key) {
+int next_hop(char key[40]) {
 	int index = 0;
 
-	if (key > self) {
-		while(key > finger_table[index + 1] && index < max_index) {
+	if (greaterThanHash(key,  self)) {
+		while(greaterThanHash(key, finger_table[index + 1]) && index < max_index) {
 			index++;
 //			fprintf(stderr, "key was larger than %d\n", finger_table[index]);
-			if (finger_table[index + 1] < finger_table[index]) 
+			if (greaterThanHash(finger_table[index], finger_table[index + 1])) 
 				break;
                 }
-		return finger_table[index];
+		return index;
 	}
 	/* key < self */
 		index = max_index;
-		while(key < finger_table[index] && index > 1) {
+		while(greaterThanHash(finger_table[index], key) && index > 1) {
 			index--;
 //			fprintf(stderr, "key was less than %d\n", finger_table[index]);
                 }
-		return finger_table[index];
+		return index;
 }
